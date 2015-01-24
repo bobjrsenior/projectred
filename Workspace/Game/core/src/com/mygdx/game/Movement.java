e1f175873a3dec96b419dbb159ad975fd43b8636
Haru <jaclynwoodlief@gmail.com>
2015-01-23 20:44:56 -0500

    movement stuffs
    yeah... movement is starting... i guess

--- Workspace/Game/core/src/com/mygdx/game/Movement.java
+++ Workspace/Game/core/src/com/mygdx/game/Movement.java
@@ -1,5 +1,28 @@
 package com.mygdx.game;
 
-public class Movement {
+public class Movement 
+{
+	private char drt;
+	
+	public Movement()
+	{
+		
+	}
+	
+	public void move(char direction)
+	{
+		drt = direction;
+		/*
+		if (drt == 'n')
+			//move North
+		else if (drt == 's')
+			//move South
+		else if (drt == 'e')
+			//move East
+		else if (drt == 'w')
+	}
 
+	public void move(char direction) {
+	*/	
+	}
 }
