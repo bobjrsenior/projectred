24cb695b03d6f687a73671b3a4f1626b1e8a2d64
Bobjrsenior <bobjrsenior@gmail.com>
2015-01-23 23:42:56 -0500

    Launcher messed up with the merge as well...

--- Workspace/Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ Workspace/Game/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,19 +2,18 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-<<<<<<< HEAD
+import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.BasicMap;
 import com.mygdx.game.*;
 //import com.mygdx.game.MyGdxGame;
 //import com.mygdx.game.BasicMap;
-=======
-import com.mygdx.game.BasicMap;
->>>>>>> f3710f29fe0b60475a223d436aad3c2434676197
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		new LwjglApplication(new TitleScreen(), config);
 		//new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new BasicMap(), config);
 		//new LwjglApplication(new BasicMap(), config);
 	}
 }
