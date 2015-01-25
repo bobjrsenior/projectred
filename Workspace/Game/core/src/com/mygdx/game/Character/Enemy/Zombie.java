dc8d5ca41a26f8ab160a0a2ab2a75cc31be685ba
MasterMatthew <Mastamats@yahoo.com>
2015-01-25 02:26:35 -0500

    Sorted stuff

---
+++ Workspace/Game/core/src/com/mygdx/game/Character/Enemy/Zombie.java
@@ -0,0 +1,5 @@
+package com.mygdx.game.Character.Enemy;
+
+public class Zombie {
+
+}
---
+++ Workspace/Game/core/src/com/mygdx/game/Character/NPC/Alien.java
@@ -0,0 +1,18 @@
+package com.mygdx.game.Character.NPC;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class Alien extends NPC{
+
+	public Alien(float x, float y, Texture tex) {
+		super(x, y);
+		tex = new Texture("People/alienfriend.png");
+		char_index = addCharacter(this);
+	}
+	/*
+	public Alien(float x, float y) {
+		super(x, y);
+		char_index = addCharacter(this);
+	}
+	*/
+}
